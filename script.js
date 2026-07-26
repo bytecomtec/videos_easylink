f2a422c73275ee39024bffce4d730cccfca870ed
Bytecomtec <pmartinez@bytecomtec.com>
2026-07-25 23:04:51 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function ponerPantallaCompleta() {
+  const video = document.getElementById("miVideo");
+  
+  if (video.requestFullscreen) {
+    video.requestFullscreen();
+  } else if (video.webkitRequestFullscreen) { /* Safari */
+    video.webkitRequestFullscreen();
+  } else if (video.msRequestFullscreen) { /* IE11 */
+    video.msRequestFullscreen();
+  }
+}
